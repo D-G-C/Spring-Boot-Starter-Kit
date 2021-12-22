9d876dd384cf2332663d8ad9a724c1fea939d382
D-G-C <delliganeshc@gmail.com>
2021-12-22 10:33:41 +0530

    Initial commit

---
+++ src/test/java/com/sb/sk/SpringBootStarterKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.sk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
